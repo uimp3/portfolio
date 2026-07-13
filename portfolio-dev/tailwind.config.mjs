4de00b63ce4418a1130df5d017160ed0a22520fe
Edwin <edwindanilo1110@udenar.edu.co>
2026-07-13 00:38:51 -0500

    Initial structure based on templates

---
+++ portfolio-dev/tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
